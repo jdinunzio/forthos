8f08796135af87ecf2b0da9a0e5ea1c14f72281f
master <augustulus@freenet.de>
2009-12-19 17:56:32 +0100

    modified:   ext2.s

--- ext2.s
+++ ext2.s
@@ -165,4 +165,163 @@ defword WORDS,WORDS,0
 	dd EXIT		; EXIT		(return from FORTH word)
 
 
+; defword: UWIDTH ; NOT TESTED_OK
+defword UWIDTH,UWIDTH,0
+;( This word returns the width (in characters) of an unsigned number in the current base )
+;: UWIDTH	( u -- width )
+;	BASE @ /	( rem quot )
+;	?DUP IF		( if quotient <> 0 then )
+;		RECURSE 1+	( return 1+recursive call )
+;	ELSE
+;		1		( return 1 )
+;	THEN
+	dd BASE , FETCH , DIV
+	dd QDUP
+	if
+		dd UWIDTH , INCR
+	else	
+		LITN 1
+	then
+	dd EXIT		; EXIT		(return from FORTH word)
+
+
+
+; defword: U.R ; NOT TESTED_OK
+defword U.R,UDOTR,0
+;: U.R		( u width -- )
+;	SWAP		( width u )
+;	DUP		( width u u )
+;	UWIDTH		( width u uwidth )
+;	ROT		( u uwidth width )
+;	SWAP -		( u width-uwidth )
+;	( At this point if the requested width is narrower, we'll have a negative number on the stack.
+;	  Otherwise the number on the stack is the number of spaces to print.  But SPACES won't print
+;	  a negative number of spaces anyway, so it's now safe to call SPACES ... )
+;	SPACES
+;	( ... and then call the underlying implementation of U. )
+;	U.
+	dd SWAP , DUP , UWIDTH, ROT , SWAP , SUB
+	dd SPACES 
+	dd UDOT
+dd EXIT		; EXIT		(return from FORTH word)
+
+; defword: U.R ; NOT TESTED_OK
+defword .R,DOTR,0
+;(
+;	.R prints a signed number, padded to a certain width.  We can't just print the sign
+;	and call U.R because we want the sign to be next to the number ('-123' instead of '-  123').
+;)
+;: .R		( n width -- )
+;	SWAP		( width n )
+;	DUP 0< IF
+;		NEGATE		( width u )
+;		1		( save a flag to remember that it was negative | width n 1 )
+;		SWAP		( width 1 u )
+;		ROT		( 1 u width )
+;		1-		( 1 u width-1 )
+;	ELSE
+;		0		( width u 0 )
+;		SWAP		( width 0 u )
+;		ROT		( 0 u width )
+;	THEN
+;	SWAP		( flag width u )
+;	DUP		( flag width u u )
+;	UWIDTH		( flag width u uwidth )
+;	ROT		( flag u uwidth width )
+;	SWAP -		( flag u width-uwidth )
+;
+;	SPACES		( flag u )
+;	SWAP		( u flag )
+;
+;	IF			( was it negative? print the - character )
+;		'-' EMIT
+;	THEN
+;
+;	U.
+	dd SWAP ,DUP
+	dd ZLT
+	if
+		dd NEGATE 
+		LITN 1
+		dd SWAP ,ROT , DECR
+	else
+		LITN 0
+		dd SWAP , ROT
+	then
+	dd SWAP , DUP , UWIDTH , ROT , SWAP , SUB , SPACES , SWAP
+	if
+		LITN '-'
+		dd EMIT
+	then
+	dd UDOT
+	dd EXIT		; EXIT		(return from FORTH word)
+
+
+; defword: . ; NOT TESTED_OK
+defword . , DOT ,0
+;( Finally we can define word . in terms of .R, with a trailing space. )
+;: . 0 .R SPACE ;
+	LITN 0 
+	dd DOTR ,SPACE 
+	dd EXIT		; EXIT		(return from FORTH word)
+; The real U., note the trailing space. ) 
+;: U. U. SPACE ;	??
+
+; defword: ? ; NOT TESTED_OK
+defword ? , QQ ,0
+;( ? fetches the integer at an address and prints it. )
+;: ? ( addr -- ) @ . ;
+	dd FETCH , DOT
+	dd EXIT		; EXIT		(return from FORTH word)
+	
+; defword: WITHIN ; NOT TESTED_OK
+defword WITHIN , WITHIN ,0
+;( c a b WITHIN returns true if a <= c and c < b )
+;(  or define without ifs: OVER - >R - R>  U<  )
+;: WITHIN
+;	-ROT		( b c a )
+;	OVER		( b c a c )
+;	<= IF
+;		> IF		( b c -- )
+;			TRUE
+;		ELSE
+;			FALSE
+;		THEN
+;	ELSE
+;		2DROP		( b c -- )
+;		FALSE
+;	THEN
+	dd NROT , OVER , LE
+	if
+		dd GT
+		if
+			dd TRUE	
+		else
+			dd FALSE
+		then
+	else
+		dd TWODROP , FALSE
+	then
+	dd EXIT		; EXIT		(return from FORTH word)
+
+; defword: ALIGNED ; NOT TESTED_OK
+defword ALIGNED , ALIGNED ,0
+;	ALIGNED takes an address and rounds it up (aligns it) to the next 4 byte boundary.
+;: ALIGNED	( addr -- addr )
+;	3 + 3 INVERT AND	( (addr+3) & ~3 )
+	LITN 3
+	dd ADD 
+	LITN 3
+	dd INVERT ,AND
+	dd EXIT		; EXIT		(return from FORTH word)
+	
+; defword: ALIGN ; NOT TESTED_OK rename !!!
+defword ALI , ALI ,0
+;ALIGN aligns the HERE pointer, so the next word appended will be aligned properly.
+;: ALIGN HERE @ ALIGNED HERE ! ;
+	dd HERE , FETCH 
+	dd ALIGNED 
+	dd HERE , STORE
+	dd EXIT		; EXIT		(return from FORTH word)
+	
 %include "ext3.s"
